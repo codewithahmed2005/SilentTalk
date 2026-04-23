d84e41c4295ac0495b58ed1c1a0d44365e257e32
codewithahmed2005 <codewithahmed2005@gmail.com>
2026-04-23 06:03:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -379,6 +379,7 @@ def serve_upload(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 # ─── Run ────────────────────────────────────────────────────────
+init_db()
+
 if __name__ == '__main__':
-    init_db()
     app.run(host='0.0.0.0', port=5000, debug=True)
